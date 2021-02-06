1d245c859adddbc5ef151ebe88ef5e82eee5fdc6
Daniel R Gorgonha <rogergorgonha@gmail.com>
2021-02-06 15:10:08 -0400

    Insert database

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -3,8 +3,12 @@ import axios from 'axios'
 export const ApiBling = axios.create({
   baseURL: 'https://bling.com.br/Api/v2',
   params: {
-    commom: {
+    common: {
       apikey: process.env.bling_apikey
     }
   }
+})
+
+export const ApiPipeDrive = axios.create({
+  baseURL: `https://${process.env.pipedrive_company}.pipedrive.com/api/v1`
 })
