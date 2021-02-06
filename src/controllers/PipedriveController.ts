2b8ed13de9655e729de8b552641994af499a37fb
Daniel R Gorgonha <rogergorgonha@gmail.com>
2021-02-06 18:44:24 -0400

    Alteração da forma de busca da API do pipedrive

--- src/controllers/PipedriveController.ts
+++ src/controllers/PipedriveController.ts
@@ -9,52 +9,150 @@ import Opportunities from '@models/Opportunities'
 class PipedriveController {
 
   public async create(req: Request, res: Response): Promise<Response> {
-    const { current: { person_name, org_name, status, value } }: IData = req.body
+    const { current: { org_id, value } }: IData = req.body
 
     //Insert database
+    await insertDB(value)
+
+    //Get details of a deal
+    const getDetails = await getDeals(org_id)
+
+    //List products attached to a deal
+    const listAllProducts = await getProducts(org_id)
+
+    //Bling request post
     try {
-      await Opportunities.create({
-        valor_total: value
-      })
-    } catch (err) {
-      throw err
-    }
-
-    //Capture contact data
-    let personName = ''
-    let personPhone = ''
-    let personEmail = ''
-
-    try {
-      const { data: { data: { items } } } = await ApiPipeDrive.get('/persons/search', {
-        params: {
-          api_token: process.env.pipedrive_token,
-          term: person_name
-        }
-      })
-
-      personName = items[0].item.name
-      personPhone = items[0].item.phones[0]
-      personEmail = items[0].item.emails[0]
 
     } catch (err) {
-      throw err.response.data
+      throw JSON.parse(err.response)
     }
 
 
-
-
-    return res.status(200).json([personName, personPhone, personEmail])
+    return res.status(200).json()
   }
+
 }
 
 export default new PipedriveController()
 
 interface IData {
   current: {
-    person_name: string
-    org_name: string
-    status: string
+    org_id: number
     value: number
   }
+}
+
+
+const insertDB = async (value: number) => {
+  try {
+    await Opportunities.create({
+      valor_total: value
+    })
+  } catch (err) {
+    throw err
+  }
+}
+
+interface IPersons {
+  data: {
+    data: {
+      org_id: {
+        name: string
+        address: string
+      },
+      person_id: {
+        name: string
+        phone: [
+          {
+            label: string
+            value: string
+            primary: boolean
+          }
+        ]
+        email: [
+          {
+            label: string
+            value: string
+            primary: boolean
+          }
+        ]
+      }
+    }
+  }
+}
+
+const getDeals = async (id: number) => {
+  try {
+    const { data: {
+      data: {
+        org_id: { name: company, address },
+        person_id: { name: person, phone, email }
+      } } }: IPersons = await ApiPipeDrive.get(`/deals/${id}`, {
+      params: {
+        api_token: process.env.pipedrive_token
+      }
+    })
+
+    return {
+      company,
+      address,
+      person,
+      phone: phone.map((phone) => phone.primary ? phone.value : '').toString(),
+      email: email.map((email) => email.primary ? email.value : '').toString()
+    }
+
+  } catch (err) {
+    throw err
+  }
+}
+
+const getProducts = async (id: number) => {
+  try {
+    return await ApiPipeDrive.get(`/deals/${id}/products`, {
+      params: {
+        api_token: process.env.pipedrive_token
+      }
+    })
+  } catch (err) {
+    throw err
+  }
+}
+
+const createXML = (data: any) => {
+  return `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <pedido>
+      <cliente>
+        <nome>${data}</nome>
+        <fone>${data}</fone>
+        <email>${data}</email>
+      </cliente>
+      <itens>
+        <item>
+          <codigo>001</codigo>
+          <descricao>Caneta 001</descricao>
+          <un>Pç</un>
+          <qtde>10</qtde>
+          <vlr_unit>1.68</vlr_unit>
+        </item>
+      </itens>
+      <parcelas>
+        <parcela>
+          <data>01/09/2009</data>
+          <vlr>100</vlr>
+          <obs>Teste obs 1</obs>
+        </parcela>
+        <parcela>
+          <data>06/09/2009</data>
+          <vlr>50</vlr>
+          <obs></obs>
+        </parcela>
+        <parcela>
+          <data>11/09/2009</data>
+          <vlr>50</vlr>
+          <obs>Teste obs 3</obs>
+        </parcela>
+      </parcelas>
+    </pedido>
+  `
 }
